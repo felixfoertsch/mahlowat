ac9a029d0165ef42385a8f67d88f4e415ed77752
Felix Förtsch <felixfoertsch@gmail.com>
2017-07-28 16:26:00 +0200

    update FAQ, fix some responsive issues

--- faq.php
+++ faq.php
@@ -34,8 +34,9 @@
   <script src="js/mahlowat.js"></script>
   
   <div class="container" style="margin-top: 20px;">
-      <img class="img-responsive pull-right" src="img/euromat.png" title="EUromat Logo"/>
-	<p id="spruch" class="pull-right"></p>
+      <div class="col-md-4 pull-right">
+      <a href="https://jef-sachsen.de/euromat"><img class="img-responsive" src="img/euromat.png" title="Euromat Logo" onclick="changeText()"/></a>
+      </div>
       <div class="bottom-buffer top-buffer">
     
     <h1>FAQ</h1>
@@ -46,23 +47,23 @@
     <p>Die Thesen wurden von den Mitgliedern der Jungen Europäischen Förderalisten Sachsen erarbeitet.</p>
     
     <h4>Wo kommen die Positionen der Gruppen her?</h4>
-    <p>Den an der Wahl teilnehmenden Parteien wurden die Thesen mit der Bitte um Stellungnahme weitergeleitet. Neben der reinen Positionierung (Zustimmung/Neutral/Ablehnung/Keine Stellungnahme) konnten sie ihre Position auch in einem kurzen Absatz erläutern.</p>
+    <p>Den an der Wahl teilnehmenden Parteien wurden die Thesen mit der Bitte um Stellungnahme weitergeleitet. Neben ihrer reinen Positionierung (also Zustimmung, Neutral, Ablehnung oder Keine Stellungnahme) konnten sie ihre Position auch in einem kurzen Absatz erläutern. Diese Stellungnahmen findet werden auf der Ergebnis-Seite angezeigt.</p>
     <p>Für ihre Stellungnahmen zu den Thesen sind die Parteien selbst verantwortlich.</p>
     
     <h4>Von welcher Wahl reden wir hier überhaupt?</h4>
     <p>Hier dreht sich alles um <a href="https://de.wikipedia.org/wiki/Bundestagswahl_2017">die Bundestagswahl 2017</a>.</p>
     
     <h4>Wer hat das hier programmiert?</h4>
-    <p>Der <a href="http://hszemi.de" title="hszemi.de" target="_blank">Sven</a>, weil der das kann.</p>
+    <p>Der <a href="http://hszemi.de" title="hszemi.de" target="_blank">Sven</a>, weil der das kann. Die EDVler von der JEF Sachsen haben noch ein paar Sachen verändert und verbessert. Zum Beispiel könnt ihr den EUromat nun auch schön auf euren Handys verwenden.</p>
     
     <h4>Funktioniert das hier wie der "echte" Wahl-O-Mat der bpb?</h4>
     <p>Es wurde versucht, die Punkteberechnung so wie beim "echten" Wahl-O-Mat zu gestalten.</p>
     
     <h4>Wie werden die Punkte berechnet?</h4>
-    <p>Die Antworten der Testperson (das bist du) werden mit den vorgegebenen Antworten der Gruppen abgeglichen.</p>
+    <p>Die Antworten der Testperson (das bist Du) werden mit den vorgegebenen Antworten der Gruppen abgeglichen.</p>
     <ul>
-      <li>Stimmt die Antwort überein, werden der Gruppe 2 Punkte gutgeschrieben;</li>
-      <li>Weicht die Antwort leicht ab (Zustimmung/Neutral oder Neutral/Ablehnung), wird der Gruppe 1 Punkt gutgeschrieben;</li>
+      <li>Stimmt die Antwort überein, werden der Gruppe 2 Punkte gutgeschrieben.</li>
+      <li>Weicht die Antwort leicht ab (Zustimmung/Neutral oder Neutral/Ablehnung), wird der Gruppe 1 Punkt gutgeschrieben.</li>
       <li>Sind die Antworten entgegengesetzt oder hat eine Gruppe eine Frage nicht beantwortet, gibt es keine Punkte für die Gruppe.</li>
     </ul>
     <p>Eine Frage, die die Testperson übersprungen hat, wird nicht gewertet. Entsprechend gibt es dann insgesamt weniger Punkte zu erreichen.</p>
@@ -70,19 +71,16 @@
     
     <div id='log'>
     <h4>Werden meine Antworten gespeichert?</h4>
-    <p>Vor der Auswertung wirst du gefragt, ob wir deine Antworten speichern dürfen.<br>
-    Du kannst dann "Ja" auswählen, was bedeutet, dass wir deine Antwortkombination zusammen mit einer ID speichern, die aus deiner IP-Adresse und einem täglich wechselnden zufälligen Wert errechnet wird. Bereits am nächsten Tag kann dein Eintrag auf keinen Fall mehr einer konkreten IP-Adresse zugeordnet werden.<br>
-    Wenn du "Nein" auswählst, wird lediglich ein Zähler um 1 erhöht.</p>
-    <p></p>
+    <p>Vor der Auswertung wirst Du gefragt, ob wir Deine Antworten speichern dürfen. Du kannst dann "Ja" auswählen, was bedeutet, dass wir Deine Antwortkombination zusammen mit einer ID speichern, die aus Deiner IP-Adresse und einem täglich wechselnden zufälligen Wert errechnet wird. Bereits am nächsten Tag kann Dein Eintrag auf keinen Fall mehr einer konkreten IP-Adresse zugeordnet werden. Wenn Du "Nein" auswählst, wird lediglich ein Zähler um 1 erhöht.</p>
     
     <h4>Ich möchte gar nicht gezählt werden!</h4>
-    <p>Unser Webserver legt bei jedem Seitenaufruf einen Log-Eintrag an, der unter anderem einen Zeitstempel, deine IP-Adresse und die aufgerufene URL enthält. Es wäre also unaufrichtig, dir vorzumachen, dass dein Aufruf des Mahlowat nicht gezählt wird. Der Mahlowat wurde jedoch so konzipiert, dass aus den Server-Logdateien nicht ersichtlich ist, welche Antworten du ausgewählt hast. Dies sehen wir tatsächlich nur, wenn du dem am Ende explizit zustimmst.</p>
+    <p>Unser Webserver legt bei jedem Seitenaufruf einen Log-Eintrag an, der unter anderem einen Zeitstempel, Deine IP-Adresse und die aufgerufene URL enthält. Es wäre also unaufrichtig, dir vorzumachen, dass Dein Aufruf des Mahlowat nicht gezählt wird. Der Mahlowat wurde jedoch so konzipiert, dass aus den Server-Logdateien nicht ersichtlich ist, welche Antworten Du ausgewählt hast. Dies sehen wir tatsächlich nur, wenn Du dem am Ende explizit zustimmst.</p>
     </div>
     
     <h4>Ich habe einen Fehler gefunden!</h4>
-    <p>Dann solltest du das melden. Wir freuen uns über sachdienliche Hinweise.</p>
+    <p>Dann solltest Du das <a href="mailto:edv@jef-sachsen.de">an uns</a> melden. Wir freuen uns über sachdienliche Hinweise.</p>
 
-    <a class="btn btn-primary" href="<?php echo $back; ?>" onclick="callPage(event, '<?php echo $back; ?>')" title="Zurück zum Mahlowat">Zurück zum Mahlowat</a>
+    <a class="btn btn-primary" href="<?php echo $back; ?>" onclick="callPage(event, '<?php echo $back; ?>')" title="Zurück zum Mahlowat">Zurück</a>
   </div>
   </div>
 
--- includes/elements.php
+++ includes/elements.php
@@ -2,26 +2,39 @@
 
 function print_result_detail_table($answers, $data){
 	$theses_count = sizeof($data['theses']);
-	for($i = 0; $i < sizeof($data['lists']); $i = $i + 1){
+
+	echo "<tr id='tableheader'>";
+	echo "<th class='hidden-xs hidden-sm'>Deine Wahl</th>";
+	for($i = 0; $i < sizeof($data['lists']); $i = $i + 1) {
 		$classname = string_to_css_classname($data['lists'][$i]['name']);
-		echo "<th class='hidden-xs hidden-sm list-$classname'>{$data['lists'][$i]['name_x']} (".calculate_points($data['answers'][$i], $answers).")</th>";   
+		echo "
+      <th class='hidden-xs hidden-sm list-$classname'>
+         {$data['lists'][$i]['name_x']} (".calculate_points($data['answers'][$i], $answers).")
+      </th>";   
 	}
 	echo "</tr>\n";
 	
 	for($i = 0; $i < $theses_count; $i = $i + 1){
-		char_to_multiply($answers[$i])==2 ? $star = '<span class="glyphicon glyphicon-star" title="Doppelte Gewichtung"></span>' : $star = '';
-		char_to_multiply($answers[$i])==2 ? $tdcl = ' class="warning"' : $tdcl = '';
+		char_to_multiply($answers[$i]) == 2 ? $star = '<i class="fa fa-star" aria-hidden="true"></i> ' : $star = '';
+		char_to_multiply($answers[$i]) == 2 ? $tdcl = ' class="info"' : $tdcl = '';
 		$labelclass = code_to_labelclass(char_to_value($answers[$i]));
 		echo "<tr$tdcl>\n";
-		echo '<td><p class="text-center">'.$star.'</p></td>';
-		echo '<td><a id="thesis'.$i.'" class="btn '.code_to_btnclass(char_to_value($answers[$i])).' btn-block" onclick="toggleNext(this)">'.$data['theses'][$i]['s'].'</a></td>';
+		echo '
+      <td>
+         <a id="thesis'.$i.'" class="btn '.code_to_btnclass(char_to_value($answers[$i])).' btn-block" onclick="toggleNext(this)">'.$star.$data['theses'][$i]['s'].'</a>
+      </td>';
 		for($listid = 0; $listid < sizeof($data['lists']); $listid = $listid + 1){
 			echo get_list_result_td($data, $listid, $i);
 		}
 		echo "</tr>\n";
 		
 		// Statements
-		echo "<tr class='multheseslong'><td class='mtl'></td><td class='mtl' colspan='".(sizeof($data['lists'])+1)."'><!--<span class='label $labelclass'>These ".($i+1).": ".$data['theses'][$i]['s']."</span><br>--> <p class='well'>".$data['theses'][$i]['l']."</p>";
+		echo "
+      <tr class='multheseslong'>
+      <td class='mtl' colspan='".(sizeof($data['lists'])+1)."'>
+      <!-- <span class='label $labelclass'>These ".($i+1).": ".$data['theses'][$i]['s']."</span><br>-->
+      <p class='well'>".$data['theses'][$i]['l']."</p>
+      <p><strong>Statements der Parteien:</strong></p>";
 		for($listid = 0; $listid < sizeof($data['lists']); $listid = $listid + 1){
 			echo get_list_statement($data, $listid ,$i);
 		}
@@ -39,15 +52,19 @@ function print_list_result_bar($data, $listindex, $answers, $class){
 		$list_percentage = 0;
 	}
 	
-	echo "<tr class='$class'>
-	<td><b>$list_name</b></td><td>$list_points von $ach_points</td>
-	<td><div class='progress'>
-		<div class='progress-bar' role='progressbar' aria-valuenow='$list_points' aria-valuemin='0' aria-valuemax='$ach_points' style='width: $list_percentage%;'>
-			$list_percentage %
-		</div>
-	</div></td>
-	</tr>";
-     
+	echo "
+   <tr class='$class'>
+   	<td><b>$list_name</b></td>
+      <td>$list_points von $ach_points</td>
+   	<td>
+         <div class='progress'>
+   		   <div class='progress-bar' role='progressbar' aria-valuenow='$list_points' aria-valuemin='0' aria-valuemax='$ach_points' style='width: $list_percentage%;'>
+   			$list_percentage %
+   		   </div>
+   	   </div>
+      </td>
+   </tr>";
+
 }
 
 /* unused
@@ -66,129 +83,129 @@ function print_list_result_bar_tricolore($list, $votes, $emph, $class){
       </div>
      </td>
      </tr>";
-}*/
+   }*/
 
-function get_list_result_td($data, $listid, $thesisid){
-	$vote = char_to_value($data['answers'][$listid][$thesisid]['selection']);
-	$listclass = "list-".string_to_css_classname($data['lists'][$listid]['name']);
-	
-	if($vote === 'skip'){
-		return "<td class='hidden-xs hidden-sm $listclass'><a class='btn btn-default btn-block disabled listanswer' >-</a></td>\n";
-	}
-	if($vote == 1){
-		return "<td class='hidden-xs hidden-sm $listclass'><a class='btn btn-success btn-block disabled listanswer' ><span class='glyphicon glyphicon-thumbs-up'></span></a></td>\n";
-	}
-	if($vote == 0){
-		return "<td class='hidden-xs hidden-sm $listclass'><a class='btn btn-warning btn-block disabled listanswer' ><span class='glyphicon glyphicon-tree-deciduous'></span></a></td>\n";
-	}
-	if($vote == -1){
-		return "<td class='hidden-xs hidden-sm $listclass'><a class='btn btn-danger btn-block disabled listanswer' ><span class='glyphicon glyphicon-thumbs-down'></i></a></td>\n";
-	}
-}
+   function get_list_result_td($data, $listid, $thesisid){
+   	$vote = char_to_value($data['answers'][$listid][$thesisid]['selection']);
+   	$listclass = "list-".string_to_css_classname($data['lists'][$listid]['name']);
 
-    
-function get_list_statement($data, $listid, $thesisid){
-	$vote  = char_to_value($data['answers'][$listid][$thesisid]['selection']);
-	$etext = $data['answers'][$listid][$thesisid]['statement'];
-	$name  = $data['lists'][$listid]['name'];
-	$listclass = "list-".str_replace(' ','',$data['lists'][$listid]['name']);
-	$prefix = "";
-	
-	if($vote === 'skip'){
-		$prefix = "<span class='label label-default'>$name</span>\n";
-	}
-	elseif($vote == 1){
-		$prefix = "<span class='label label-success'>$name</span>\n";
-	}
-	elseif($vote == 0){
-		$prefix = "<span class='label label-warning'>$name</span>\n";
-		}
-	elseif($vote == -1){
-		$prefix = "<span class='label label-danger'>$name</span>\n";
-	}
-	
-	return "<div class='$listclass'>
-	$prefix 
-	<p>$etext</p>
-	</div>\n\n";
-}
+   	if($vote === 'skip'){
+   		return "<td class='hidden-xs hidden-sm $listclass'><a class='btn btn-default btn-block listanswer' >-</a></td>\n";
+   	}
+   	if($vote == 1){
+   		return "<td class='hidden-xs hidden-sm $listclass'><a class='btn btn-success btn-block listanswer' ><i class='fa fa-thumbs-up fa-lg'></i></a></td>\n";
+   	}
+   	if($vote == 0){
+   		return "<td class='hidden-xs hidden-sm $listclass'><a class='btn btn-warning btn-block listanswer' ><i class='fa fa-asterisk fa-lg'></i></a></td>\n";
+   	}
+   	if($vote == -1){
+   		return "<td class='hidden-xs hidden-sm $listclass'><a class='btn btn-danger btn-block listanswer' ><i class='fa fa-thumbs-down fa-lg'></i></a></td>\n";
+   	}
+   }
 
-function code_to_btnclass($int){
-	if($int === 'skip'){
-		return 'btn-default';
-	}
-	if($int == 1){
-		return 'btn-success';
-	}
-	if($int == 0){
-		return 'btn-warning';
-	}
-	if($int == -1){
-		return 'btn-danger';
-	}
-}
-    
-function code_to_labelclass($int){
-	if($int === 'skip'){
-		return '';
-	}
-	if($int == 1){
-		return 'label-success';
-	}
-	if($int == 0){
-		return 'label-warning';
-	}
-	if($int == -1){
-		return 'label-important';
-	}
-}
-    
-function pagitem($i, $curr){
-	if($i == $curr){
-		return '<li class="active"><a href="#">'.$i."</a></li>\n";
-	} else {
-		return '<li class=""><a href="mahlowat.php?id='.$i.'">'.$i."</a></li>\n";
-	}
-}
 
-function print_pagination($theses_count){
-	echo '<ul id="navigation" class="pagination pagination-sm">';
-	for($i = 1; $i < ($theses_count+1); $i = $i + 1){
-		echo "<li><a href='#$i' onclick='loadThesis($i)'>$i</a></li>";
-	}
-	echo '</ul>';
-}
+   function get_list_statement($data, $listid, $thesisid){
+   	$vote  = char_to_value($data['answers'][$listid][$thesisid]['selection']);
+   	$etext = $data['answers'][$listid][$thesisid]['statement'];
+   	$name  = $data['lists'][$listid]['name'];
+   	$listclass = "list-".str_replace(' ','',$data['lists'][$listid]['name']);
+   	$prefix = "";
 
-function print_thesesbox($theses, $form=false, $list=null){
-	echo '<div id="thesesbox">';
-	
-	for($q_id = 0; $q_id < count($theses); $q_id++){
-		echo "<div id='thesis$q_id' class='singlethesis'>";
-		echo "<h1>".($q_id+1)."/".(count($theses))."  ".$theses[$q_id]['s']."</h2>
+   	if($vote === 'skip'){
+   		$prefix = "<span class='label label-default'>$name</span>\n";
+   	}
+   	elseif($vote == 1){
+   		$prefix = "<span class='label label-success'>$name</span>\n";
+   	}
+   	elseif($vote == 0){
+   		$prefix = "<span class='label label-warning'>$name</span>\n";
+   	}
+   	elseif($vote == -1){
+   		$prefix = "<span class='label label-danger'>$name</span>\n";
+   	}
 
-		<div class='well well-large statement'>
-		<p style='margin-bottom: 0px;' class='lead'>";
-		
-		echo $theses[$q_id]['l'];
-		echo "</p>";
-		if($theses[$q_id]['x'] != ''){
-			echo "<button class='btn btn-link explanationbutton'>Erklärung</button>\n";
-			echo "<div class='explic'>".$theses[$q_id]['x']."</div>";
-		}
-		echo "</div>";
-		
-		if($form){
-			$input = $list['comments'][$q_id];
-			echo "<div class='row'>
-			<div class='col-xs-12 col-sm-12 col-md-8 col-md-offset-2'>
-				<textarea id='input-$q_id' name='comments[$q_id]' class='form-control' rows='3' placeholder='Hier die Begründung eingeben...'>$input</textarea>
-			</div>
-			</div>";
-		}
-		
-		echo "</div>";
-	}
-	echo '</div>';
-}
+   	return "<div class='$listclass'>
+   	$prefix 
+   	<p>$etext</p>
+   </div>\n\n";
+ }
 
-?>
+ function code_to_btnclass($int){
+ 	if($int === 'skip'){
+ 		return 'btn-default';
+ 	}
+ 	if($int == 1){
+ 		return 'btn-success';
+ 	}
+ 	if($int == 0){
+ 		return 'btn-warning';
+ 	}
+ 	if($int == -1){
+ 		return 'btn-danger';
+ 	}
+ }
+
+ function code_to_labelclass($int){
+ 	if($int === 'skip'){
+ 		return '';
+ 	}
+ 	if($int == 1){
+ 		return 'label-success';
+ 	}
+ 	if($int == 0){
+ 		return 'label-warning';
+ 	}
+ 	if($int == -1){
+ 		return 'label-important';
+ 	}
+ }
+
+ function pagitem($i, $curr){
+ 	if($i == $curr){
+ 		return '<li class="active"><a href="#">'.$i."</a></li>\n";
+ 	} else {
+ 		return '<li class=""><a href="mahlowat.php?id='.$i.'">'.$i."</a></li>\n";
+ 	}
+ }
+
+ function print_pagination($theses_count){
+ 	echo '<ul id="navigation" class="pagination pagination-sm">';
+ 	for($i = 1; $i < ($theses_count+1); $i = $i + 1){
+ 		echo "<li><a href='#$i' onclick='loadThesis($i)'>$i</a></li>";
+ 	}
+ 	echo '</ul>';
+ }
+
+ function print_thesesbox($theses, $form=false, $list=null){
+ 	echo '<div id="thesesbox">';
+
+ 	for($q_id = 0; $q_id < count($theses); $q_id++){
+ 		echo "<div id='thesis$q_id' class='singlethesis'>";
+ 		echo "<h1>".$theses[$q_id]['s']."</h1>
+
+ 		<div class='well well-large statement'>
+ 			<p style='margin-bottom: 0px;' class='lead'>";
+
+ 				echo $theses[$q_id]['l'];
+ 				echo "</p>";
+ 				if($theses[$q_id]['x'] != ''){
+ 					echo "<button class='btn btn-link explanationbutton'>Erklärung</button>\n";
+ 					echo "<div class='explic'>".$theses[$q_id]['x']."</div>";
+ 				}
+ 				echo "</div>";
+
+ 				if($form){
+ 					$input = $list['comments'][$q_id];
+ 					echo "<div class='row'>
+ 					<div class='col-xs-12 col-sm-12 col-md-8 col-md-offset-2'>
+ 						<textarea id='input-$q_id' name='comments[$q_id]' class='form-control' rows='3' placeholder='Hier die Begründung eingeben...'>$input</textarea>
+ 					</div>
+ 				</div>";
+ 			}
+
+ 			echo "</div>";
+ 		}
+ 		echo '</div>';
+ 	}
+
+ 	?>
